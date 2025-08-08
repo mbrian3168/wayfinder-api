ca73bfaf5f423efdcacd4c5bd1689f5f777166ee
mbrian3168 <mbrian3168@gmail.com>
2025-08-07 21:44:10 -0400

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,9 @@
-import app from '../src/server';
-export default app;
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import app from '../src/app';
+
+// Vercel will invoke this function per request.
+// We pass the request into the Express app instance.
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  // @ts-expect-error Express compatibility
+  return app(req, res);
+}
